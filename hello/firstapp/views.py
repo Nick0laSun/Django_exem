5c7f90b44a6228301d320d2f7243259662423581
Nick0laSun <37973338+Nick0laSun@users.noreply.github.com>
2020-04-03 11:12:46 +0300

    Initial commit

---
+++ hello/firstapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "firstapp/home.html")
+    # return HttpResponse("Hello World!")
+    
+def index(request):
+    header = "Personal Data"
+    langs = ["English", "German", "Spanish"]
+    user = {"name": "Tom", "age": 23}
+    addr = ("Абрикосовая", 23, 45)
+
+    data = { "header": header, "langs": langs, "user": user, "address": addr }
+    return render(request, "index.html", context=data)
+    # return render(request, "index.html")
